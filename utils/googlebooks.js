3ed5d955751c3af3ad2a0ee3845dc160cb6e3b41
CameronGastineau <CameronGastineau@users.noreply.github.com>
2020-02-02 17:36:50 -0700

    Added header file and ready for submission

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -18,17 +18,3 @@ module.exports = async (phrase) => {
 
     return JSON.stringify(jsonData);
 }
-
-/*  console.log(results.data);
-    console.log(results.status);
-    console.log(results.statusText);
-    console.log(results.headers);
-
-        const results = await axios({
-        method: 'get',
-        url: 'https://www.googleapis.com/books/v1/volumes',
-        params: {
-            format: 'json',
-            q: `${phrase}`,
-        },
-    })*/
